71a6c36ae077543469d8bbf8da2baa8fc1335987
Zenna Tavares <zennatavares@gmail.com>
2018-05-17 21:17:24 -0400

    rt

--- src/render.jl
+++ src/render.jl
@@ -14,7 +14,7 @@ dot_self(x) = dot_(x, x)
 dot_(xs, ys) = sum(xs .* ys)
 
 "normalized x: `x/norm(x)`"
-simplenormalize(x::Vector) = x / sqrt(dot_self(x))
+simplenormalize(x) = x / sqrt(dot_self(x))
 
 "x iff x > 0 else 0"
 rlu(x) = max(zero(x), x)
@@ -78,11 +78,20 @@ end
 "Position where ray hits object"
 hitposition(r::Ray, tnear) = r.orig + r.dir * tnear 
 
+using ZenUtils
+
 "Normal between `r` and `sphere`"
 function normal(hitpos, sphere::Sphere, tnear::Real)
-  nhit = hitpos - center(sphere)
+  nhit = hitpos .- center(sphere)
   nhit = simplenormalize(nhit)
 end
+
+# using Flux 
+# using ForwardDiff
+# function Base.:-(a::ForwardDiff.Dual{Tx, V, N}, b::Flux.Tracker.TrackedReal) where {Tx, N, V <: Real}
+#   # @assert false
+#   Flux.Tracker.track(Base.:-, a, b)
+# end
 
 "Light contribution from all objects in scene"
 function light(scene::Scene, geom::Geometry, hitpos, nhit, bias = 1e-4)
@@ -91,7 +100,7 @@ function light(scene::Scene, geom::Geometry, hitpos, nhit, bias = 1e-4)
     if scene[i].emission_color[1] > 0.0 # scene[i] is a light
       transmission = 1.0
       light_dir = scene[i].center - hitpos  # FIXME: Don't have this
-      light_dir = normalize(light_dir)
+      light_dir = simplenormalize(light_dir)
 
       for j = 1:length(scene)
         if (i != j)
@@ -185,13 +194,13 @@ function render(scene::Scene,
                 width::Integer=100,
                 height::Integer=100,
                 fov::Real=30.0,
-                trc=trc)
+                trc=trc,
+                image = zeros(width, height, 3))
   inv_width = 1.0 / width
   angle = tan(pi * 0.5 * fov / 100.0)
   inv_height = 1.0 / height
   aspect_ratio = width / height
-
-  image = zeros(width, height, 3)
+ 
   for y = 1:height
     for x = 1:width
       xx = (2 * ((x + 0.5) * inv_width) - 1.0) * angle * aspect_ratio
@@ -203,4 +212,32 @@ function render(scene::Scene,
     end
   end
   image
+end
+
+"Generate ray dirs and ray origins"
+function rdirs_rorigs(width::Integer=200,
+                      height::Integer=200,
+                      fov::Real=30.0)
+  inv_width = 1 / width
+  angle = tan(pi * 0.5 * fov / 100.0)
+  inv_height = 1 / height
+  aspect_ratio = width / height
+
+  image = zeros(width, height, 3)
+  rdirs = Array{Float64}(width * height, 3)
+  rorigs = Array{Float64}(width * height, 3)
+  j = 1
+  for y = 1:height, x = 1:width
+    xx = (2 * ((x + 0.5) * inv_width) - 1) * angle * aspect_ratio
+    yy = (1 - 2 * ((y + 0.5) * inv_height)) * angle
+    minus1 = -1.0
+    raydir = simplenormalize(Vec3([xx, yy, -1.0]))
+    rorig = Vec3([0.0, 0.0, 0.0])
+    rdirs[j, :] = raydir
+    rorigs[j, :] = rorig
+    # pixel = trc(Ray(Vec3([0.0, 0.0, 0.0]), raydir), spheres, 0)
+    # image[x, y, :] = pixel
+    j += 1
+  end
+  rdirs, rorigs
 end
