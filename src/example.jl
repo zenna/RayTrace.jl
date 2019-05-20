9d40a94cb2e1131aa243e6d9e77909f637bcce3a
Zenna Tavares <zennatavares@gmail.com>
2019-05-20 03:35:52 -0400

    make optional

--- src/example.jl
+++ src/example.jl
@@ -1,12 +1,15 @@
 "Some example spheres which should create actual image"
-function example_spheres()
-  scene = [msphere(Point(0.0, -10004, -20), 10000.0, Vec3(0.20, 0.20, 0.20), 0.0, 0.0, Vec3(0.0, 0.0, 0.0)),
+function example_spheres(; addfloor = true)
+  scene = [
            msphere(Point(0.0, 0.0, -20), 4.0, Vec3(1.0, 0.32, 0.36), 1.0, 0.5, zeros(Vec3)),
            msphere(Point(5.0, 1.0, -15), 2.0, Vec3(0.90, 0.76, 0.46), 1.0, 0.0, zeros(Vec3)),
            msphere(Point(5.0, 0.0, -25), 3.0, Vec3(0.65, 0.77, 0.970), 1.0, 0.0, zeros(Vec3)),
            msphere(Point(-5.5,      0, -15), 3.0, Vec3(0.90, 0.90, 0.90), 1.0, 0.0, zeros(Vec3)),
            msphere(Point(0.0, 20.0, -30), 3.0, zeros(Vec3), 0.0, 0.0, Vec3(3.0, 3.0, 3.0))]
-RayTrace.ListScene(scene)
+  if addfloor
+    push!(scenes, msphere(Point(0.0, -10004, -20), 10000.0, Vec3(0.20, 0.20, 0.20), 0.0, 0.0, Vec3(0.0, 0.0, 0.0)))
+  end
+  RayTrace.ListScene(scene)
 end
 
 
